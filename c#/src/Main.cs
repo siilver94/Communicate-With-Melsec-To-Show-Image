60c3e22e85079c97a54fb7a87f316c60faa3f541
Eun Sung Allen Kim <siiilver9@gmail.com>
2022-06-27 09:41:26 +0900

    Update Main.cs
    Add convert.Hex() funciotn to show word data .

--- c#/src/Main.cs
+++ c#/src/Main.cs
@@ -17,14 +17,14 @@ namespace GetSignalShowImage
 {
     public partial class Main : Form
     {
-        
+
         private delegate void dele();
 
         int currentImage = 1;
         int i;
 
         TcpClient tc;
-        
+
         TCPClient_K client;
 
         int NumOfImage = 4;
@@ -54,29 +54,29 @@ namespace GetSignalShowImage
 
             return DateTime.Now;
         }
-        //private void ShowImage(int value)        //이미지 출력
-        //{
+        private void ShowImage(int value)        //이미지 출력
+        {
 
-        //    PictureBox[] picture = new PictureBox[]
-        //    {
-        //     pictureBox1,pictureBox2,pictureBox3,pictureBox4
-        //    };
+            PictureBox[] picture = new PictureBox[]
+            {
+             pictureBox1,pictureBox2,pictureBox3,pictureBox4
+            };
+
+            for (int i = 0; i < NumOfImage; i++)
+            {
+                picture[i].Load(@"C:\Users\Vector\source\repos\GetSignalShowImage\" + (value + i ) + ".jpg");
+                picture[i].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
             
-        //    for (int i = 0; i < NumOfImage; i++)
-        //    {
-        //        picture[i].Load(@"C:\Users\Vector\source\repos\GetSignalShowImage\" + (value + i) + ".jpg");
-        //        picture[i].SizeMode = PictureBoxSizeMode.StretchImage;
-        //    }
 
-        //}
+        }
         private void ChngBtn_Click(object sender, EventArgs e)   //체인지 버튼
         {
-            
-          //  ShowImage(1);
-                       
+
+            ShowImage(1);
+
         }
-
-
+        
 
         void ClientStart()
         {
@@ -94,20 +94,33 @@ namespace GetSignalShowImage
             MessageBox.Show("접속되었습니다.");
             if (currentImage == 1)
             {
-              // ShowImage(0);
+                ShowImage(1);
             }
 
         }
 
-        public static string ByteSubstring(string Data, int StartIndex, int ByteLength)
+
+        public static string ConvertHex(String hexString)
         {
-            String str = "";
+            try
+            {
+                string ascii = string.Empty;
 
-            byte[] TEMP = Encoding.Default.GetBytes(Data);
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
 
-            str = Encoding.Default.GetString(TEMP, StartIndex, ByteLength);
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+                }
 
-            return str;
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
         }
 
         private void button2_Click(object sender, EventArgs e)  //접속 종료
@@ -117,30 +130,30 @@ namespace GetSignalShowImage
 
         }
 
-      
+
 
         void client_TalkingComm(string name, object data, string data2, string data3, string data4, string data5, string data6, string data7, string data8, string data9)  //데이터 받기
         {
-            
+
 
             this.Invoke(new dele(() =>
             {
 
                 //byte[] bt = (byte[])data;
-                
+
 
                 if (name.Equals("Image2"))
                 {
                     if (Convert.ToInt32(data2) == 1)
                     {
-                        
-                     //  ShowImage(2);
+
+                          ShowImage(2);
                     }
 
                     if (Convert.ToInt32(data2) == 2)
                     {
-                        
-                     //   ShowImage(3);
+
+                           ShowImage(3);
                     }
                 }
 
@@ -152,35 +165,16 @@ namespace GetSignalShowImage
                     string barcode3 = data4;
                     string barcode4 = data5;
                     string barcode5 = data6;
-                   
 
                     string Wdata1 = data7;
                     string Wdata2 = data8;
 
-                    string bridge = "";
-                  
-                    textBox5.Text = barcode1 + bridge + barcode2 + bridge + barcode3 + bridge + barcode4 + bridge + barcode5 + bridge;
+                    textBox5.Text = barcode1 + barcode2 + barcode3 + barcode4 + barcode5;
 
-                    //int code1 = Convert.ToInt32(Wdata1);
-                    //int code2 = Convert.ToInt32(Wdata2);
-
-                    //char Ccode1 = Convert.ToChar(Wdata1);
-                    //char Ccode2 = Convert.ToChar(Wdata2);
-
-                    int intWdata1 = Convert.ToInt32(Wdata1);
-                    int intWdata2 = Convert.ToInt32(Wdata2);
-
-                    string strWdata1 = Convert.ToString(intWdata1, 2);
-                    string strWdata2 = Convert.ToString(intWdata2, 2);
-
-                    //textBox6.Text = Convert.ToString(a, 2) + Convert.ToString(b, 2); ;
-
-                    string sub1, sub2;
-
-                    sub1 = ByteSubstring(strWdata1, 1, 9);
-                    sub2 = ByteSubstring(strWdata2, 10, 9);
-
-                    textBox6.Text = sub1.ToString();
+                    string a, b;
+                    a = Convert.ToString(Convert.ToInt32(Wdata1), 16);
+                    b = Convert.ToString(Convert.ToInt32(Wdata2), 16);
+                    textBox6.Text = ConvertHex(a) + " " + ConvertHex(b);
 
                 }
 
@@ -201,12 +195,12 @@ namespace GetSignalShowImage
             client.MCWrite_D(1003, 0);
         }
 
-        
+
 
         private void button5_Click(object sender, EventArgs e)      //데이터 + 바코드 합치기
         {
             string result;
-            result = textBox6.Text + textBox5.Text;
+            result = textBox6.Text + " " + textBox5.Text;
             textBox7.Text = result;
         }
 
